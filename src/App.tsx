25573df3425a3e22362906fb545e268cb0ae8dcc
Rodrigo Fernandes <rodrigo-fernands1@hotmail.com>
2025-08-19 21:35:12 -0300

    primeiras alterações

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+//Pascal case
+//App
+//HeaderHeading
 function App() {
-  const [count, setCount] = useState(0)
+  console.log('Oi');
 
   return (
-    <>
+    <h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1> Ola </h1>
+        <p> Bem-vindo ao Chronos Pomodoro! </p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </h1>
+  );
 }
 
-export default App
+export { App };
